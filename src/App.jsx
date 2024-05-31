9352f43850c97b33b7b852953ef2af4b738850d4
banderGlitch <nernaykumar98@gmail.com>
2024-05-31 17:09:44 +0530

    accordian comp

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,19 @@
-
+import Header from "./component/Header/Header";
+import Hero from "./component/Hero/Hero";
+import './App.css';
+import Residencies from "./component/Residencies/Residencies";
+import Companies from "./component/Companies/Companies";
 function App() {
   return (
-    <span>subscribe Zainkeepscode</span>
+    <div className="App">
+      <div>
+        <Header />
+        <Hero />
+      </div>
+      <div className="white-gradient" />
+      <Companies />
+      <Residencies/>
+    </div>
   );
 }
 
---
+++ src/component/Hero/Hero.jsx
@@ -0,0 +1,63 @@
+import React from 'react'
+import './Hero.css';
+import { HiLocationMarker } from "react-icons/hi";
+import CountUp from "react-countup";
+
+export default function Hero() {
+    return (
+        <section className='hero-wrapper'>
+            <div className="paddings innerWidth hero-container flexCenter">
+                {/*Left Side */}
+                <div className="flexColStart hero-left">
+                    <div className="hero-title">
+                        <div className='orange-circle' />
+                        <h1>
+                            Discover <br /> Most Suitable <br /> Property
+                        </h1>
+                    </div>
+                    <div className="flexColStart hero-des">
+                        <span className='secondaryText'>
+                            Find a variety of properties that suit you very easilty
+                        </span>
+                        <span className='secondaryText'>
+                            Forget all difficulties in finding a residence for you
+                        </span>
+                    </div>
+                    <div className="flexCenter search-bar">
+                        <HiLocationMarker color="var(--blue)" size={25} />
+                        <input type='text' />
+                        <button className='button'>Search</button>
+                    </div>
+                    <div className="flexCenter stats">
+                        <div className="flexColStart stat">
+                            <span>
+                                <CountUp start={8800} end={9000} duration={4} /> <span>+</span>
+                            </span>
+                            <span className="secondaryText">Premium Product</span>
+
+                        </div>
+                        <div className="flexColStart stat">
+                            <span>
+                                <CountUp start={1950} end={2000} duration={4} /> <span>+</span>
+                            </span>
+                            <span className="secondaryText">Happy Customer</span>
+                        </div>
+                        <div className="flexColStart stat">
+                            <span>
+                                <CountUp end={28} /> <span>+</span>
+                            </span>
+                            <span className="secondaryText">Awards Winning</span>
+                        </div>
+                    </div>
+                </div>
+                {/*Right Side */}
+                <div className="flexCenter hero-right">
+
+                    <div className="image-container">
+                        <img src='./hero-image.png' alt='hero-img' />
+                    </div>
+                </div>
+            </div>
+        </section>
+    )
+}
