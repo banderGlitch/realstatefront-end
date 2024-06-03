66b06bc2da74407db47ecaece14e6956106dbe08
banderGlitch <nernaykumar98@gmail.com>
2024-06-03 14:13:14 +0530

    accordian component added

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import Hero from "./component/Hero/Hero";
 import './App.css';
 import Residencies from "./component/Residencies/Residencies";
 import Companies from "./component/Companies/Companies";
+import Value from "./component/Value/Value";
 function App() {
   return (
     <div className="App">
@@ -13,6 +14,7 @@ function App() {
       <div className="white-gradient" />
       <Companies />
       <Residencies/>
+      <Value/>
     </div>
   );
 }
